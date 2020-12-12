88b77ab5d520eab2716e5dbf1460f98dc8c31ef7
razaqa <dhafins18@gmail.com>
2020-12-12 21:50:41 +0700

    Add hello world app

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('');
+  const [textGreeting, setTextGreeting] = useState('Hello, tell me your name!');
+ 
+  const textChangedHandler = (enteredText) => {
+    setName(enteredText);
+  };
+ 
+  const buttonClickedHandler = () =>{
+    setTextGreeting("Hello, " + name + "!");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{textGreeting}</Text>
+      <View style = {styles.inputContainer}>
+        <TextInput
+          placeholder = "Name"
+          style = {styles.input}
+          onChangeText = {textChangedHandler}
+          value = {name}
+        />
+      </View>
+      <Button
+        title = "Submit"
+        onPress = {buttonClickedHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +41,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input :{
+    borderColor:"black", 
+    borderWidth:1,
+    borderRadius:20,
+    padding:5,
+    width:200,
+    height:30,
+  },
+  inputContainer :{
+    flexDirection :'row', 
+    justifyContent :'space-between', 
+    alignContent:'center',
+    bottom:20,
+    paddingTop:40,
   },
 });
